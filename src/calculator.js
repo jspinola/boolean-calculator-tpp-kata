f5d66e20b645e4083b42b40b0ccf4a5f8a7486a7
Jesús Spínola <jesus.spinola@lifullconnect.com>
2021-02-28 00:22:42 +0100

    initial kata rename

---
+++ src/calculator.js
@@ -0,0 +1,5 @@
+const calculator = () => {
+  return true
+}
+
+module.exports = calculator
---
+++ src/calculator.test.js
@@ -0,0 +1,7 @@
+const calculator = require('./calculator')
+
+describe('calculator should', () => {
+  it('return true', () => {
+    expect(calculator()).toBe(true)
+  })
+})
